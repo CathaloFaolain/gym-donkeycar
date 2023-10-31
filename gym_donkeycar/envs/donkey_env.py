54d653d89c6965a71ed37bb17ff25f2fc8822b34
CathaloFaolain <64092214+CathaloFaolain@users.noreply.github.com>
2023-10-31 16:24:43 +0000

    Updated cte

--- gym_donkeycar/envs/donkey_env.py
+++ gym_donkeycar/envs/donkey_env.py
@@ -28,7 +28,7 @@ def supply_defaults(conf: Dict[str, Any]) -> None:
     """
     defaults = [
         ("start_delay", 5.0),
-        ("max_cte", 8.0),
+        ("max_cte", 3.0),
         ("frame_skip", 1),
         ("cam_resolution", (120, 160, 3)),
         ("log_level", logging.INFO),
